93565b2a0deb7a11efa7ea0d863ec199b47ec65c
MagicalMysteryRaxo <zodtra.raxo@gmail.com>
2026-06-03 19:32:32 -0600

    version 0.0.6 quest generator

--- js/gens.js
+++ js/gens.js
@@ -1,107 +1,120 @@
 const npcs = [
   "Your next door neighbor",
-  "Your best friend",
+  "A very suspicious witch",
   "Your enemy",
   "One of your siblings",
-  "Your spouse",
-  "Your coworker",
-  "Your boss",
+  "A tiny gnome",
+  "One of your coworkers",
+  "Someone who looks suspiciously like you",
   "A weird little kid",
-  "A policeman",
-  "A ghost",
+  "A sketchy policeman",
+  "A literal ghost",
   "A talking dog",
-  "A blue Alien",
+  "A blue alien",
   "A random stranger you've never seen before",
-  "The Watcher",
+  "God",
 ];
 
 const actions = [
-  "runs to you screaming. They ask you to ",
+  "runs to you screaming and waving their hands. They grab you and insist you have to ",
   "texts you out of the blue. They want you to ",
-  "shows up at your door. They need you to ",
+  "shows up at your door. When you open they keep saying you need to ",
   "speaks to you in a dream. They tell you to ",
-  "offers you a deal. They absolutely need you to ",
-  "communicates with you telepathically. They demand you to ",
-  "asks you for help. They want you to ",
-  "looks you dead in the eyes. They say you must ",
+  "asks you softly if you could ",
+  "communicates with you telepathically. They demand you ",
+  "hands you a letter soup with a message. They want you to ",
+  "looks you dead in the eye. They say you must by any means necessary ",
   "slips you a note. It says you should ",
-  "whispers to you in the middle of the night. They want you to ",
-  "is staring at you from across the street. They want you to ",
+  "whispers to you in the middle of the night. They absolutely need you to ",
+  "is staring at you from across the street. They mouth the words that you have to ",
 ];
 
 const quests = {
   romance: [
-    "go on a date with the first Sim you see.",
-    "reach 50 relationship points with a stranger.",
-    "kiss someone before midnight.",
-    "rekindle an old romance.",
-    "break up a couple.",
-    "become engaged within 3 days.",
-    "go on a date to the cemetery.",
-    "kiss 5 people in 5 days.",
-    "woohoo in a hot tub.",
+    "go on a date with the first Sim you see ",
+    "reach 50 relationship points with a stranger ",
+    "kiss someone before midnight ",
+    "break up with your partner ",
+    "break up a couple ",
+    "become engaged ",
+    "go on a date to the cemetery ",
+    "kiss 5 people ",
+    "woohoo in a hot tub ",
   ],
   family: [
-    "host a family dinner.",
-    "have a three generation gathering.",
-    "teach a child a new skill.",
-    "take a family photo.",
-    "visit a relative you haven't seen recently.",
-    "throw a birthday party.",
-    "adopt a child.",
-    "start a new family tradition.",
-    "go out to the park with your family.",
+    "host a family dinner ",
+    "have a three generation gathering ",
+    "teach a child a new skill ",
+    "take a family photo ",
+    "visit a relative you haven't seen recently ",
+    "throw a birthday party ",
+    "adopt a child ",
+    "start a new family tradition ",
+    "go out to the park with your family ",
   ],
   career: [
-    "earn a promotion.",
-    "make friends with a coworker.",
-    "gain the skill needed for your next promotion.",
-    "buy something with your earnings.",
-    "become self-employed.",
-    "change careers completely.",
-    "prank a coworker.",
+    "earn a promotion ",
+    "make friends with a coworker ",
+    "gain the skill needed for your next promotion ",
+    "buy something with your earnings ",
+    "become self-employed ",
+    "change careers completely ",
+    "prank a coworker ",
   ],
   skills: [
-    "gain 2 skill points today.",
-    "learn a skill you've never touched.",
-    "reach level 5 in a skill.",
-    "maximize a skill before sunday.",
+    "gain 2 skill points today ",
+    "learn a skill you've never touched ",
+    "reach level 5 in a skill ",
+    "maximize a skill before sunday ",
   ],
   drama: [
-    "start a fight with a random Sim.",
-    "help create 2 best friends.",
-    "create 2 enemies.",
-    "have a public argument.",
-    "become friends with an enemy.",
-    "break up a friendship.",
-    "kill your biggest enemy.",
-    "have a romantic relationship with an enemy.",
-    "resurrect a dead Sim.",
-    "steal a Sim's partner.",
+    "start a fight with a random Sim ",
+    "help create 2 best friends ",
+    "create 2 enemies ",
+    "have a public argument ",
+    "become friends with an enemy ",
+    "break up some random friendship ",
+    "kill your biggest enemy ",
+    "have a romantic relationship with an enemy ",
+    "resurrect a dead Sim ",
+    "steal a Sim's partner ",
   ],
   rivalry: [
-    "beat your rival in a skill.",
-    "have more money than your rival.",
-    "win a fight.",
-    "looksmaxx and become the most attractive Sim in the neighborhood.",
-    "forgive your enemy and become friends.",
+    "beat your rival in a skill ",
+    "have more money than your rival ",
+    "win a fight ",
+    "looksmaxx and become the most attractive Sim in the neighborhood ",
+    "forgive your enemy and become friends ",
   ],
   chaos: [
-    "quit your job.",
-    "move houses.",
-    "invite a random stranger to live with you.",
-    "have cosmetic surgery.",
-    "spend all your money.",
-    "sell your most expensive object.",
-    "become enemies with your best friend.",
-    "throw a party tonight no matter what.",
-    "rearrange your living room.",
-    "rearrange your entire home.",
-    "paint your house a new color.",
-    "paint your hair a new color.",
-    "dress only in your favorite color.",
+    "quit your job ",
+    "move houses ",
+    "invite a random stranger to live with you ",
+    "have cosmetic surgery ",
+    "spend all your money ",
+    "sell your most expensive object ",
+    "become enemies with your best friend ",
+    "throw a party tonight no matter what ",
+    "rearrange your living room ",
+    "rearrange your entire home ",
+    "paint your house a new color ",
+    "paint your hair a new color ",
+    "dress only in your favorite color ",
+    "get a new tattoo ",
+    "get a new piercing ",
   ],
 };
+
+const when = [
+  "today.",
+  "tonight.",
+  "before the week is over.",
+  "before the season ends.",
+  "in 5 days.",
+  "in 2 days.",
+  "in one week.",
+  "before sun up.",
+];
 
 const rewards = [
   "you get +1000 simoleons",
@@ -109,13 +122,16 @@ const rewards = [
   "you get a new stereo",
   "you gain one free skill point",
   "you get a new outfit",
-  "you get a career performance boost",
+  "you get a career promotion",
   "you get a free room makeover",
-  "you can have a free makeover",
+  "they will give you a free makeover",
   "they will give you a puppy",
   "they will install a new hot tub in your backyard",
   "they will construct a new room in your house",
   "they will pay for a vacation for you and your family",
+  "you get a new computer",
+  "you get +10,000 simoleons",
+  "you get a new couch",
 ];
 
 const consequences = [
@@ -133,7 +149,11 @@ const consequences = [
   "they will curse you with eternal bad luck",
   "they will make your skin blue",
   "they will make your skin purple",
+  "you will grow horns",
 ];
+
+let currentReward = "";
+let currentConsequence = "";
 
 function getRandomItem(array) {
   const randomIndex = Math.floor(Math.random() * array.length);
@@ -148,18 +168,41 @@ function rollQuest() {
   const questType = getRandomItem(questTypes);
 
   const quest = getRandomItem(quests[questType]);
-  const reward = getRandomItem(rewards);
-  const consequence = getRandomItem(consequences);
+  currentReward = getRandomItem(rewards);
+  currentConsequence = getRandomItem(consequences);
 
+  const whenItem = getRandomItem(when);
   const resultBox = document.getElementById("questResult");
 
   resultBox.innerHTML = `
     <h2>Your Quest</h2>
-    <p> ${npc} ${action} ${quest}</p>
-    <p><strong>If you do: </strong> ${reward}</p>
-    <p><strong>If you don't: </strong> ${consequence}</p>
-    <p><strong>Quest Type:</strong> ${questType}</p>
+    <p class="quest-description"> ${npc} ${action} ${quest} ${whenItem}</p>
     
+   
+    
+    <div class="quest-buttons">
+      <button class="gen-btn results" id="successBtn">I did it</button>
+      <button class="gen-btn results" id="failBtn">I failed</button>
+    </div>
+
+    <div id="questOutcome"></div>
+  `;
+
+  document.getElementById("successBtn").addEventListener("click", showReward);
+  document.getElementById("failBtn").addEventListener("click", showConsequence);
+}
+
+function showReward() {
+  document.getElementById("questOutcome").innerHTML = `
+    <h3>Reward</h3>
+    <p>${currentReward}</p>
+  `;
+}
+
+function showConsequence() {
+  document.getElementById("questOutcome").innerHTML = `
+    <h3>Consequence</h3>
+    <p>${currentConsequence}</p>
   `;
 }
 
